408f5e61ef72ffa3e50670e2cbd30580c81c476c
Rushikesh Bhattad <rushikeshbhattad8@gmail.com>
2021-08-30 14:39:05 +0530

    Add project files.

---
+++ ConsoleAppShorthand/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ShorthandOperatorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = 10;
+            b = 3;
+            a %= b;
+            Console.WriteLine("a= " + a);
+            a = 3;
+            b = 2;
+            a *= 2;
+            Console.WriteLine("a= " + a);
+            a = 27;
+            b = 3;
+            a /= b;
+            Console.WriteLine("a= " + a);
+            a = 97;
+            b = 89;
+            a -= b;
+            Console.WriteLine("a= " + a);
+            a = 97;
+            b = 107;
+            a -= b;
+            Console.WriteLine("a= " + a);
+            Console.ReadKey();
+        }
+    }
+}
